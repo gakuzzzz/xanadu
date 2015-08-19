b1ad0551663fa88faf6a672f2ba061f5a516191a
gakuzzzz <gaku_github@t2v.jp>
2015-08-20 00:53:48 +0900

    Added chunked

--- src/main/java/jp/t2v/xanadu/ops/Spliterators.java
+++ src/main/java/jp/t2v/xanadu/ops/Spliterators.java
@@ -1,13 +1,16 @@
 package jp.t2v.xanadu.ops;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Spliterator;
+import java.util.Spliterators.AbstractSpliterator;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 class Spliterators {
 
-    static class TakeWhileSpliterator<T> extends java.util.Spliterators.AbstractSpliterator<T> {
+    static class TakeWhileSpliterator<T> extends AbstractSpliterator<T> {
         private final Spliterator<? extends T> spliterator;
         private final Predicate<? super T> predicate;
         private boolean running = true;
@@ -35,7 +38,7 @@ class Spliterators {
     }
 
 
-    static class ZipSpliterator<A, B, C> extends java.util.Spliterators.AbstractSpliterator<C> {
+    static class ZipSpliterator<A, B, C> extends AbstractSpliterator<C> {
         private final Spliterator<A> a;
         private final Spliterator<B> b;
         private final BiFunction<? super A, ? super B, ? extends C> mapper;
@@ -59,6 +62,35 @@ class Spliterators {
                 return aHasNext && bHasNext;
             }
             return false;
+        }
+    }
+
+    static class ChunkedSpliterator<E> extends AbstractSpliterator<List<E>>
+    {
+        private final Spliterator<E> spliterator;
+        private final int partitionSize;
+
+        public ChunkedSpliterator(Spliterator<E> toWrap, int partitionSize) {
+            super(toWrap.estimateSize(), toWrap.characteristics() | Spliterator.NONNULL);
+            if (partitionSize <= 0) throw new IllegalArgumentException(
+                    "Partition size must be positive, but was " + partitionSize);
+            this.spliterator = toWrap;
+            this.partitionSize = partitionSize;
+        }
+
+        @Override public boolean tryAdvance(Consumer<? super List<E>> action) {
+            final List<E> partition = new ArrayList<>(partitionSize);
+            while (spliterator.tryAdvance(partition::add)
+                    && partition.size() < partitionSize);
+            if (partition.isEmpty()) return false;
+            action.accept(partition);
+            return true;
+        }
+
+        @Override public long estimateSize() {
+            final long est = spliterator.estimateSize();
+            return est == Long.MAX_VALUE? est
+                    : est / partitionSize + (est % partitionSize > 0? 1 : 0);
         }
     }
 
--- src/test/java/jp/t2v/xanadu/ops/StreamOpsTest.java
+++ src/test/java/jp/t2v/xanadu/ops/StreamOpsTest.java
@@ -11,6 +11,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -31,6 +32,18 @@ public class StreamOpsTest {
         List<Message> messages = Arrays.asList(new Message(Optional.of("a")), new Message(Optional.empty()));
         List<Message> messages2 = messages.stream().toList();
         assertThat(messages, is(messages2));
+    }
+
+    @Test
+    public void chunked() {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        List<List<Integer>> result = list.stream().chunked(3).toList();
+        assertThat(result, is(Arrays.asList(
+                Arrays.asList(1, 2, 3),
+                Arrays.asList(4, 5, 6),
+                Arrays.asList(7, 8, 9),
+                Arrays.asList(10))
+        ));
     }
 
 }
